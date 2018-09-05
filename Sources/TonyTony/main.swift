60ccda858544ef90a3473f296974a2c9467b70df
Алексей Лысенко <abesmon@gmail.com>
2018-09-05 22:57:27 +0300

    display

--- Sources/TonyTony/main.swift
+++ Sources/TonyTony/main.swift
@@ -1,5 +1,45 @@
 import Foundation
 
+
+import PCD8544
+import SwiftyGPIO
+
+extension Color {
+    var grayscale: Color {
+        let middle = (red + green + blue) / 3
+        return Color(red: middle, green: middle, blue: middle, alpha: alpha)
+    }
+}
+
+class NokScreen: Screen {
+    var pixelSize: GSize { return GSize(width: LCDWIDTH, height: LCDHEIGHT) }
+    
+    private let hardwDisplay: PCD8544
+    init?() {
+        let gpios = SwiftyGPIO.GPIOs(for: .OrangePiZero)
+        guard let rst = gpios[.P26],
+            let dc = gpios[.P22],
+            let cs = gpios[.P24],
+            let spi = SwiftyGPIO.hardwareSPIs(for: .OrangePiZero)?.first
+            else { return nil }
+        self.hardwDisplay = PCD8544(spi: spi, dc: dc, rst: rst, cs: cs)
+    }
+    
+    func drawPixel(atPoint point: GPoint, color: Color) {
+        let graysc = color.grayscale
+        let lcdColor: LCDColor = graysc.red > (255/2) ? .WHITE : .BLACK
+        hardwDisplay.setPixel(x: point.x, y: point.y, color: lcdColor)
+    }
+    
+    func display() {
+        hardwDisplay.display()
+    }
+    
+    func clear() {
+        hardwDisplay.clearDisplay()
+    }
+}
+
 class FooViewController: ViewController {
     private var greenView: View!
     private var blueView: View!
@@ -48,6 +88,7 @@ func start() {
     blue.alpha = 0.1
     let blended = red.blend(withColor: blue).toCGColor()
     let application = Application.shared
+    application.mainScreen = NokScreen()
     let mainAppDelegate = MainAppDelegate()
     application.delegate = mainAppDelegate
     application.runMainLoop()
